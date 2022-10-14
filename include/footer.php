80dafad64cb4e575de245cf364cd8ea7ac8d1561
Alex <alexis.amand@gmail.com>
2022-10-14 13:08:27 +0200

    [update] Ajout du lien Tipeee

--- include/footer.php
+++ include/footer.php
@@ -1,5 +1,5 @@
 <div class="col-md-12 text-center small mt-5">
-Copyright © 2018-<?php echo date('Y'); ?><br />
+Copyright © 2018-<?php echo date('Y'); ?> | <a href="https://fr.tipeee.com/histoires-de-poilus" title="Soutenir le site sur Tipeee">Soutenir le projet sur Tipeee</a><br />
 Développé par <a href="https://www.boitasite.com" title="Alexis AMAND développeur web/seo à Valenciennes">Alexis AMAND</a> - <a href="https://github.com/AlexisAmand/Imerologio">Page GitHub du projet</a> - version <?php echo SITE_VERSION; ?> 
 </div>
 
