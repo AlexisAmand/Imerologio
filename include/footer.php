cdc753671c803cac2394dd1a85b1006df1e729d4
Alex <alexis.amand@gmail.com>
2023-01-06 14:09:42 +0100

    [maj] plop

--- include/footer.php
+++ include/footer.php
@@ -2,5 +2,3 @@
 Copyright © 2018-<?php echo date('Y'); ?> | <a href="https://fr.tipeee.com/histoires-de-poilus" title="Soutenir le site sur Tipeee">Soutenir le projet sur Tipeee</a><br />
 Développé par <a href="https://www.boitasite.com" title="Alexis AMAND développeur web/seo à Valenciennes">Alexis AMAND</a> - <a href="https://github.com/AlexisAmand/Imerologio">Page GitHub du projet</a> - version <?php echo SITE_VERSION; ?> 
 </div>
-
-
